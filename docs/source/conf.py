fe753bab6fcd5f901af9f44297e9a9a75546f37d
Charles C. D. Tolley <charlescdtolley@protonmail.com>
2024-11-23 00:42:47 -0700

    Publish version 1.0.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 project = "versioning"
 copyright = "2024, Charles C. D. Tolley"
 author = "Charles C. D. Tolley"
-version = "1.0.0.0"
+version = "1.0.0.1"
 release = version
 html_theme = "python_docs_theme"
